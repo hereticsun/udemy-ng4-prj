83afb8826acd13df207b456652a19f35fd2ea1d9
Azlan Cuttilan <azlan@bynd.com>
2017-06-15 14:52:26 +0100

    152 Adding editing routes
    Signed-off-by: Azlan Cuttilan <azlan@bynd.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
@@ -12,8 +13,10 @@ const appRoutes: Routes = [
         path: 'recipes',
         component: RecipesComponent,
         children: [
-        { path: '', component: RecipeStartComponent},
-        { path: ':id', component: RecipeDetailComponent},
+            { path: '', component: RecipeStartComponent},
+            { path: 'new', component: RecipeEditComponent},
+            { path: ':id', component: RecipeDetailComponent},
+            { path: ':id/edit', component: RecipeEditComponent},
         ] 
     },
     { path: 'shopping-list', component: ShoppingListComponent },
